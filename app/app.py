e6edaf3e1e0654ac5c2df705d9a1b71b00c49cfd
stormlighter72 <cocorich72@gmail.com>
2023-01-20 10:41:28 +0000

    Ajout "Modification Films"

--- app/app.py
+++ app/app.py
@@ -64,3 +64,59 @@ def AddFilm2():
         with open('video2.json','w+') as videotheque_file:
                 json.dump(videotheque, videotheque_file, indent=4)
                 return "Ok"
+        
+@app.post('/library1')
+def DelFilm():
+        titre = request.json['titre']
+        videotheque = None
+
+        with open('video.json') as videotheque_file :
+                videotheque = json.load(videotheque_file)
+                index = next((i for i, film in enumerate(videotheque['films']) if film['titre'] == titre), None)
+                if index is not None :
+                        del videotheque['films'][index]
+                else :
+                        abort(404) 
+        with open('video.json','w+') as videotheque_file:
+                json.dump(videotheque, videotheque_file, indent=4)
+                return "Ok"
+
+@app.route("/modiffilm")
+def modif():
+    titre = request.args.get("titre")
+    with open("video.json") as videotheque_file:
+        videotheque = json.load(videotheque_file)
+        film = next((film for film in videotheque['films'] if film['titre'] == titre), None)
+
+    if film:
+        return film
+    else:
+        return jsonify({"error": "Film not found"})
+
+@app.route("/mod",methods=['POST'])
+def mod():
+    titre_base = request.json['titre_base']
+    titre = request.json['titre']
+    annee = request.json['annee']
+    nom_realisateur = request.json['nom_realisateur']
+    prenom_realisateur = request.json['prenom_realisateur']
+    nom_acteur1 = request.json['nom_acteur1']
+    nom_acteur2 = request.json['nom_acteur2']
+    nom_acteur3 = request.json['nom_acteur3']
+    prenom_acteur1 = request.json['prenom_acteur1']
+    prenom_acteur2 = request.json['prenom_acteur2']
+    prenom_acteur3 = request.json['prenom_acteur3']
+    with open('video.json', 'r') as videotheque_file :
+        videotheque = json.load(videotheque_file)
+        for i, film in enumerate(videotheque['films']):
+            if titre_base == film['titre']:
+                del videotheque['films'][i]
+                break
+    with open('video.json','w+') as videotheque_file:
+        json.dump(videotheque, videotheque_file, indent=4)
+    with open('video.json') as videotheque_file :
+        videotheque = json.load(videotheque_file)
+        videotheque["films"].append({"titre":titre,"annee":annee, "realisateur":{"nom":nom_realisateur,"prenom":prenom_realisateur},"acteurs":[{"nom":nom_acteur1,"prenom":prenom_acteur1},{"nom":nom_acteur2,"prenom":prenom_acteur2},{"nom":nom_acteur3,"prenom":prenom_acteur3}]})
+    with open('video.json','w+') as videotheque_file:
+        json.dump(videotheque, videotheque_file, indent=4)
+        return "Ok niquel"
--- web/app.py
+++ web/app.py
@@ -76,6 +76,52 @@ def AddFilm2():
         else:
             return response.text
     
+    
+@app.route('/library1', methods = ["GET", "POST"])
+def DelFilm():
+    if request.method == "GET" :
+        return render_template("library.html.jinja")
+    else:
+        delete = request.form['delete']
+        
+        response = requests.post(app_url+"library1", json={"titre":delete})
+        if response.status_code == 200 :
+            return redirect(url_for("Library_1"))
+        else:
+            return response.text
+
+@app.route('/modiffilm', methods = ["GET", "POST"])
+def ModifFilm():
+    if request.method == "GET" :
+        titre = request.args["modif"]
+        response = requests.get("http://app:5000/modiffilm?titre=" + titre)
+        showfilm = response.json()
+        
+        if "error" in showfilm:
+        # Display an error message if the film was not found
+            return response.txt
+        else:
+        # Display the film data if it was found
+            return render_template("modiffilm.html.jinja", film=showfilm)
+    else:
+        titre_base = request.form['titre_base']
+        titre = request.form['titre']
+        annee = request.form['annee']
+        nom_realisateur = request.form['nom_realisateur']
+        prenom_realisateur = request.form['prenom_realisateur']
+        nom_acteur1 = request.form['nom_acteur1']
+        nom_acteur2 = request.form['nom_acteur2']
+        nom_acteur3 = request.form['nom_acteur3']
+        prenom_acteur1 = request.form['prenom_acteur1']
+        prenom_acteur2 = request.form['prenom_acteur2']
+        prenom_acteur3 = request.form['prenom_acteur3']
+        reponse = requests.post(app_url + "mod", json={"titre_base":titre_base,"titre":titre,"annee":annee,"nom_realisateur":nom_realisateur,"prenom_realisateur":prenom_realisateur,"nom_acteur1":nom_acteur1,"nom_acteur2":nom_acteur2,"nom_acteur3":nom_acteur3,"prenom_acteur1":prenom_acteur1,"prenom_acteur2":prenom_acteur2,"prenom_acteur3":prenom_acteur3})
+        
+        if reponse.status_code==200:
+            return redirect(url_for("Library_1"))
+        else:
+            return reponse.text
+
 
 @app.errorhandler(404)
 def error_404(error):
